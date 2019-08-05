e1a8485187b0e161cfb52c1cabf9b6ca4ef96699
ho2ri <ho2ri2s.github@gmail.com>
2019-08-05 16:55:14 +0900

    navigationを実装。

---
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ui/qrcode/QRcodeFragment.kt
@@ -0,0 +1,28 @@
+package jp.co.cyberagent.dojo2019.ui.qrcode
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import jp.co.cyberagent.dojo2019.R
+
+
+class QRcodeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_qrcode, container, false)
+    }
+}
---
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ui/user/UserFragment.kt
@@ -0,0 +1,27 @@
+package jp.co.cyberagent.dojo2019.ui.user
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import jp.co.cyberagent.dojo2019.R
+
+
+class UserFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+}
