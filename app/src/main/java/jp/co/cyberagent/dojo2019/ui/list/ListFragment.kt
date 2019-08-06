d340e0de5cc69f9b9d4196a307ec760aac34b9a3
ho2ri <ho2ri2s.github@gmail.com>
2019-08-06 10:55:31 +0900

    RecyclerViewのモック作成。DataBindなし。

--- app/src/main/java/jp/co/cyberagent/dojo2019/ui/list/ListFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ui/list/ListFragment.kt
@@ -7,14 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 
 import jp.co.cyberagent.dojo2019.R
+import jp.co.cyberagent.dojo2019.data.User
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -24,5 +28,23 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val userList = arrayListOf<User>()
+        val user1: User = User("hori yuta", "ho2ri2s", "hohohoris")
+        val user2: User = User("yamauti ran", "run", "yamauti")
+        val user3: User = User("wao", "waowao", "waowaowao")
+        val user4: User = User("aiueo", "hoho", "hohoh")
+        userList.add(user1)
+        userList.add(user2)
+        userList.add(user3)
+        userList.add(user4)
+
+        recyclerView.apply{
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = ListAdapter(userList)
+        }
     }
 }
