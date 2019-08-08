de7129c75e1489497f09357aaedf92415551db4b
ho2ri <ho2ri2s.github@gmail.com>
2019-08-08 13:32:40 +0900

    sharedPreferenceをrepositoryに追記

--- app/src/main/java/jp/co/cyberagent/dojo2019/data/repository/UserRepository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/data/repository/UserRepository.kt
@@ -11,6 +11,15 @@ interface UserRepository {
 
     fun getSearchedUsers(name: String?, githubAccount: String, twitterAccount: String?): LiveData<List<User>>
 
-    //TODO sharedPredference
+    fun readName(): String?
 
+    fun readGithubAccount(): String
+
+    fun readTwitterAccount(): String?
+
+    fun writeName(name: String?)
+
+    fun writeGithubAccount(githubAccount: String)
+
+    fun writeTwitterAccount(twitterAccount: String?)
 }
--- app/src/main/java/jp/co/cyberagent/dojo2019/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/data/repository/UserRepositoryImpl.kt
@@ -1,15 +1,21 @@
 package jp.co.cyberagent.dojo2019.data.repository
 
+import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import jp.co.cyberagent.dojo2019.data.UserDao
 import jp.co.cyberagent.dojo2019.data.entity.User
+import jp.co.cyberagent.dojo2019.di.MY_GITHUB_ACCOUNT
+import jp.co.cyberagent.dojo2019.di.MY_NAME
+import jp.co.cyberagent.dojo2019.di.MY_TWITTER_ACCOUNT
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserRepositoryImpl @Inject constructor(
-    private val userDao: UserDao
+    private val userDao: UserDao,
+    private val sharedPreferences: SharedPreferences
 ) : UserRepository {
+
 
     override fun upsertUser(user: User) {
         userDao.upsertUser(user)
@@ -23,5 +29,25 @@ class UserRepositoryImpl @Inject constructor(
         return userDao.getSearchedUsers(name, githubAccount, twitterAccount)
     }
 
+    override fun readName(): String? =
+        sharedPreferences.getString(MY_NAME, "")
+
+    override fun readGithubAccount(): String =
+        sharedPreferences.getString(MY_GITHUB_ACCOUNT, "")!!
+
+    override fun readTwitterAccount(): String? =
+        sharedPreferences.getString(MY_TWITTER_ACCOUNT, "")
+
+    override fun writeName(name: String?) {
+        sharedPreferences.edit().putString(MY_NAME, name)
+    }
+
+    override fun writeGithubAccount(githubAccount: String) {
+        sharedPreferences.edit().putString(MY_GITHUB_ACCOUNT, githubAccount)
+    }
+
+    override fun writeTwitterAccount(twitterAccount: String?) {
+        sharedPreferences.edit().putString(MY_TWITTER_ACCOUNT, twitterAccount)
+    }
 
 }
