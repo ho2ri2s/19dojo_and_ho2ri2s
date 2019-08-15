c03580b8e11db9b3eae066bda152e87719ffcd5b
ho2ri <ho2ri2s.github@gmail.com>
2019-08-15 11:56:13 +0900

    DialogFragment + Dagger2 + Navigation実装

---
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ui/qrcode/UserDialogFragment.kt
@@ -0,0 +1,61 @@
+package jp.co.cyberagent.dojo2019.ui.qrcode
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProviders
+import dagger.android.support.DaggerAppCompatDialogFragment
+import jp.co.cyberagent.dojo2019.di.ViewModelFactory
+import javax.inject.Inject
+import kotlin.random.Random
+
+//TODO DaggerDialogFragmentにしたい
+class UserDialogFragment : DaggerAppCompatDialogFragment() {
+
+
+    private lateinit var viewModel: QRcodeViewModel
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    lateinit var message: String
+
+
+    companion object {
+        fun newInstance(name: String, githubAccount: String, twitterAccount: String): UserDialogFragment {
+            val bundle = Bundle().also {
+                it.putString(
+                    "message",
+                    "name : ${name}\n" +
+                            "github account : ${githubAccount}\n" +
+                            "twitter account : ${twitterAccount}"
+                )
+            }
+            val userDialogFragment = UserDialogFragment().apply { arguments = bundle }
+            return userDialogFragment
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        message = arguments?.getString("message") as String
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        viewModel = ViewModelProviders.of(requireActivity(), viewModelFactory).get(QRcodeViewModel::class.java)
+        Log.d("TAG", "${viewModel}")
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("追加しますか？")
+            .setMessage(message)
+            .setPositiveButton("追加", { _, _ ->
+                Log.d("TAG", "ositayo")
+                viewModel.dialogOK.postValue(Unit)
+            })
+            .setNegativeButton("キャンセル", { _, _ ->
+                Log.d("TAG", "kyannseru")
+                viewModel.dialogCancel.postValue(Unit)
+            })
+        return builder.create()
+    }
+}
